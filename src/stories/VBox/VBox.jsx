0eb6ba3915a6b87ca548b9e1c5674bd1c7e5df0a
jinsu kim <67sc2@naver.com>
2022-11-15 14:47:32 +0900

    vue storybokk-test

--- src/stories/VBox/VBox.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import styled from 'styled-components';
-import {Box} from './StyledElement';
-
-const VBox = styled(Box)`
-  flex-direction: column;
-`;
-
-export default VBox;
